21d6452dbdd70ae2fe788556c396b6962d7f82d2
koopa31 <clement.cazorla31@gmail.com>
2025-02-21 18:09:11 +0100

    logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 html_logo = "_static/logo.png" 
 
 # -- Options for EPUB output
